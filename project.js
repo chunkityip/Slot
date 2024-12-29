d526e733cbbd7fe4dd164c47d34075c9c493223a
chunkityip <alex0917lfo@hotmail.com>
2024-12-29 16:00:01 -0500

    implement getNumberOfLines()

--- project.js
+++ project.js
@@ -16,8 +16,40 @@
 
 const prompt = require("prompt-sync")(); // Import thrid part library call prompt-sunc
 
+// public void deposit()
 const deposit = () => {
-    const depositAmount = prompt("Enter a deposit amount: ");
+    while (true) {
+        const depositAmout = prompt("Enter a deposit amount: ");
+        const numberDepositAmount = parseFloat(depositAmout);  // parseFlot is build in function to change String to number
+
+        if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) { //isNan() is build in function
+            console.log("Invalid deposit amount, try again.")
+        } else {
+            return numberDepositAmount;
+        }
+    }
 };
 
-deposit();
+const getNumberOfLines = () => {
+    while (true) {
+        const lines = prompt("Enter the number of lines to bet on (1 - 3): ");
+        const numberofLines = parseFloat(lines);  // parseFlot is build in function to change String to number
+
+        if (isNaN(numberofLines) || numberofLines <= 0 || numberofLines > 3) { //isNan() is build in function
+            console.log("Invalid numver of lines, try again.")
+        } else {
+            return numberofLines;
+        }
+    }
+};
+
+
+
+/**
+Scanner scanner = new Scanner(System.in);
+System.out.print("Enter a deposit amount: ");
+String depositAmount = scanner.nextLine();
+ */
+const depositAmount = deposit();
+const numberofLines = getNumberOfLines();
+
