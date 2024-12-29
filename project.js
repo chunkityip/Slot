90706b33fa9964e2a6f9efeb6dfe64a1477dbdf3
chunkityip <alex0917lfo@hotmail.com>
2024-12-29 14:03:16 -0500

    implment deposit() function

---
+++ project.js
@@ -0,0 +1,23 @@
+// 1. Despot some money
+// 2. Determine number of lines to bet on
+// 3. Collect a bet amount
+// 4. Spin the slot machine
+// 5. Check if the user won
+// 6. give the user their winnings
+// 7. Play gain
+
+// function deposit() {
+//     return 1
+// }
+
+// Don't use var
+// const is immutable
+// let is mutable
+
+const prompt = require("prompt-sync")(); // Import thrid part library call prompt-sunc
+
+const deposit = () => {
+    const depositAmount = prompt("Enter a deposit amount: ");
+};
+
+deposit();
