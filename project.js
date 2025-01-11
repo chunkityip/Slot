8f9f9934c3db7a7a21071db81cbd026c60823dfa
chunkityip <alex0917lfo@hotmail.com>
2025-01-11 06:58:45 -0500

    implement Spin ,getWinning

--- project.js
+++ project.js
@@ -40,18 +40,69 @@ const spin = () => {
         symbols.push(symbol);
       }
     }
-    const reels = [[], [], []];
-    for (let i = 0; i< COLS; i++) {
-        const reelSymbols = [...symbols];
-        for (let j = 0; j < ROWS; j++) {
-            const randomIndex = Math.floor(Math.random() * reelSymbols.length);
-                const selectedSymbol = reelSymbols[randomIndex];
-                reels[i].push(selectedSymbol);
-                reelSymbols.splice(randomIndex, 1);
-        }
+  
+    const reels = [];
+    for (let i = 0; i < COLS; i++) {
+      reels.push([]);
+      const reelSymbols = [...symbols];
+      for (let j = 0; j < ROWS; j++) {
+        const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+        const selectedSymbol = reelSymbols[randomIndex];
+        reels[i].push(selectedSymbol);
+        reelSymbols.splice(randomIndex, 1);
+      }
     }
+  
     return reels;
-};
+  };
+
+  const transpose = (reels) => {
+    const rows = [];
+  
+    for (let i = 0; i < ROWS; i++) {
+      rows.push([]);
+      for (let j = 0; j < COLS; j++) {
+        rows[i].push(reels[j][i]);
+      }
+    }
+  
+    return rows;
+  };
+  
+  const printRows = (rows) => {
+    for (const row of rows) {
+      let rowString = "";
+      for (const [i, symbol] of row.entries()) {
+        rowString += symbol;
+        if (i != row.length - 1) {
+          rowString += " | ";
+        }
+      }
+      console.log(rowString);
+    }
+  };
+
+  const getWinnings = (rows, bet, lines) => {
+    let winnings = 0;
+  
+    for (let row = 0; row < lines; row++) {
+      const symbols = rows[row];
+      let allSame = true;
+  
+      for (const symbol of symbols) {
+        if (symbol != symbols[0]) {
+          allSame = false;
+          break;
+        }
+      }
+  
+      if (allSame) {
+        winnings += bet * SYMBOL_VALUES[symbols[0]];
+      }
+    }
+  
+    return winnings;
+  };
 
 spin();
 // public void deposit()
@@ -135,5 +186,9 @@ String depositAmount = scanner.nextLine();
  */
 let balance = deposit();
 const numberofLines = getNumberOfLines();
-const get = getBet(balance, numberofLines);
-
+const reels = spin();
+const rows = transpose(reels);
+const bet = getBet(balance, numberofLines);
+printRows(rows);
+const winnings = getWinnings(rows , bet , numberofLines);
+console.log("You won, $" + winnings.toString());
